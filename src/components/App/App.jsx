45a789284e83c6788aaae7e502aa1a5469d07cee
Alexandr <alexandr.kopyletz@gmail.com>
2024-08-10 21:33:10 +0300

    10.08/1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,39 +1,44 @@
-import React from "react";
+import react from "react";
 import './App.css'
-import userDat from "../userData/userDat.json";
-
-const userData = JSON.parse(userDat);
+import userData from "../userData/userDat.json";
+import css from "./Profile.module.css"
 
 const Profile = ({ name, tag, location, image, stats }) => {
   return (
-    <div className="profile">
-      <img src={image} alt={`${name}'s avatar`} />
-      <p>{name}</p>
-      <p>@{tag}</p>
-      <p>{location}</p>
-      <ul>
-        <li>Followers: </li>
-        <li>{stats.followers}</li>
-        <li>Views: </li>
-        <li>{stats.views}</li>
-        <li>Likes: </li>
-        <li>{stats.likes}</li>
+    <div className={css.profile}>
+      <img src={image} alt={`${name}'s avatar`} className = {css.avatar}/>
+      <p className = {css.name}>{name}</p>
+      <p className = {css.tag}>@{tag}</p>
+      <p className = {css.location}>{location}</p>
+      <ul className ={css.stats}>
+        <div className={css.li}>
+        <li className = {css.label}>Followers: </li>
+        <li className = {css.quantity}>{stats.followers}</li>
+        </div>
+        <div className={css.li}>
+        <li className = {css.label}>Views: </li>
+        <li className = {css.quantity}>{stats.views}</li>
+        </div>
+        <div className={css.li}>
+        <li className = {css.label}>Likes: </li>
+        <li className = {css.quantity}>{stats.likes}</li>
+        </div>
       </ul>
     </div>
   );
 };
   
 export default function App () {
-    return (
-      <>
-        <Profile
-          image={userData.avatar}
-          name={userData.username}
-          tag={userData.tag}
-          location={userData.location}
-          stats={userData.stats}
-        />
-      </>
-    );
-  };
+  return (
+    <>
+      <Profile
+         image={userData.avatar}
+         name={userData.username}
+         tag={userData.tag}
+         location={userData.location}
+         stats={userData.stats}
+     />
+   </>
+   );
+};
   
